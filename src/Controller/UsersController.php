ca5b503aba2ecb2946c8d3138a79cfddb99f0165
Masaya Nakashima <kawa18sima@gmail.com>
2018-08-29 23:55:27 +0900

    Edit UsersController
    Add access restriction for each action to beforeFilter
    Add exception handling when user does not exist in view, edit, delete
    Various other changes

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Datasource\Exception\RecordNotFoundException;
 use Cake\Event\Event;
 
 class UsersController extends AppController
@@ -9,40 +10,51 @@ class UsersController extends AppController
 
     public function beforeFilter(Event $event){
         parent::beforeFilter($event);
-        $this->Auth->allow(['login','signup']);
+        $before_login = ['login','signup'];
+        $this->Auth->allow($before_login);
+        $user = $this->Auth->user();
+        if(in_array($this->request->action, $before_login)){
+            if(isset($user)){
+                $this->Flash->success('すでにログインしています');
+                $this->redirect(['controller'=>'Products','action'=>'index']);
+            }
+        }
+        else if($this->request->action == 'index'){
+            if($user['level'] != 2){
+                $this->Flash->error('権限がありません。');
+                return $this->redirect(['controller' => 'Products', 'action' => 'index']);
+            }
+        }
     }
 
     public function index()
     {
         $users = $this->paginate($this->Users);
-
         $this->set(compact('users'));
     }
 
     public function view($id = null)
     {
-        $user = $this->Users->get($id, [
-            'contain' => []
-        ]);
-
-        $this->set('user', $user);
+        try{
+            $user = $this->Users->get($id, [
+                'contain' => []
+            ]);
+            $this->set('user', $user);
+        }catch(RecordNotFoundException $e){
+            $this->Flash->error('ユーザが存在しません。');
+            $this->redirect(['controller'=>'Products','action'=>'index']);
+        }
     }
 
     public function login(){
-        $user = $this->Auth->user();
-        if(isset($user)){
-            $this->redirect(['controller'=>'Users','action'=>'index']);
-        }
-        else{
-            if ($this->request->is('post')){
-                $user = $this->Auth->identify();
-                if ($user) {
-                    $this->Auth->setUser($user);
-                    $this->Flash->success(__('ログインしました'));
-                    return $this->redirect($this->Auth->redirectUrl());
-                }
-                $this->Flash->error(__('ユーザ名もしくはパスワードが間違っています'));
+        if ($this->request->is('post')){
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                $this->Flash->success(__('ログインしました'));
+                return $this->redirect($this->Auth->redirectUrl());
             }
+            $this->Flash->error(__('ユーザ名もしくはパスワードが間違っています'));
         }
     }
 
@@ -68,31 +80,55 @@ class UsersController extends AppController
 
     public function edit($id = null)
     {
-        $user = $this->Users->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
-            if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
+        try{
+            $current_user = $this->Auth->user();
+            $user = $this->Users->get($id, [
+                'contain' => []
+            ]);
+            if($id != $current_user['id'] && $current_user['level'] != 2 || !isset($user)){
+                $this->redirect(['controller'=>'Products','action'=>'index']);
             }
-            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+            if ($this->request->is(['patch', 'post', 'put'])) {
+                $user = $this->Users->patchEntity($user, $this->request->getData());
+                if ($this->Users->save($user)) {
+                    $this->Flash->success(__('変更が適用されました。'));
+
+                    return $this->redirect(['action' => 'index']);
+                }
+                $this->Flash->error(__('変更されませんでした。もう一度お試しください。'));
+            }
+            $this->set(compact('user'));
+        }catch(RecordNotFoundException $e){
+            $this->Flash->error('ユーザが存在しません。');
+            $this->redirect(['controller'=>'Products','action'=>'index']);
         }
-        $this->set(compact('user'));
     }
 
     public function delete($id = null)
     {
-        $this->request->allowMethod(['post', 'delete']);
-        $user = $this->Users->get($id);
-        if ($this->Users->delete($user)) {
-            $this->Flash->success(__('The user has been deleted.'));
-        } else {
-            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
-        }
+        try{
+            $current_user = $this->Auth->user();
+            if($id != $current_user['id'] && $current_user['level'] != 2){
+                $this->redirect(['controller'=>'Products','action'=>'index']);
+            }
+            $this->request->allowMethod(['post', 'delete']);
+            $user = $this->Users->get($id);
+            $flag = $id == $current_user['id'];
+            if ($this->Users->delete($user)) {
+                if($flag){
+                    $this->Flash->success(__('退会しました。'));
+                    return $this->redirect($this->Auth->logout());
+                }else{
+                    $this->Flash->success(__('ユーザを削除しました。'));
+                }
+            } else {
+                $this->Flash->error(__('エラーが起きました。'));
+            }
 
-        return $this->redirect(['action' => 'index']);
+            return $this->redirect(['action' => 'index']);
+        }catch(RecordNotFoundException $e){
+            $this->Flash->error('ユーザが存在しません。');
+            $this->redirect(['controller'=>'Products','action'=>'index']);
+        }
     }
 }
