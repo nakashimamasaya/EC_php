c70c1de11492709cb3e10e99c755c0baf580efbc
Masaya Nakashima <kawa18sima@gmail.com>
2018-09-03 02:13:49 +0900

    Creating an intermediate table
    Create a migration
    Performing migration

---
+++ config/Migrations/20180902171220_CreateCategoriesProduct.php
@@ -0,0 +1,28 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateCategoriesProduct extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('categories_product');
+        $table->addColumn('category_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('product_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
