9f48d2d11a10d6c09b5502af637971927254e3db
Masaya Nakashima <kawa18sima@gmail.com>
2018-09-03 00:52:12 +0900

    Editing the cart controller
    Add processing that can not be added when there is no stock
    
    Add processing when a negative number is entered in the number to buy
    
    Added function to delete from cart when number to buy is 0

--- src/Controller/CartsController.php
+++ src/Controller/CartsController.php
@@ -29,6 +29,7 @@ class CartsController extends AppController
     public function add($id = null)
     {
         $this->loadModel("Purchases");
+        $this->loadModel("Products");
         $this->autoRender = false;
         $cart = $this->Carts->newEntity();
         if($this->request->is('post')){
@@ -40,13 +41,17 @@ class CartsController extends AppController
                 'count' => 1
                 ]);
             $carts = $this->Carts->find('all')->where(['user_id' => $cart->user_id, 'product_id' => $id, 'purchase_id' => $cart->purchase_id])->first();
+            $product = $this->Products->get($id);
             if(isset($carts)){
                 $this->Flash->error('カートに追加済みです');
+            }
+            else if($product->stock == 0){
+                $this->Flash->error('在庫がありません');
             }
             else if ($this->Carts->save($cart)) {
                 $this->Flash->success('カートに追加されました。');
             }
-            else {
+            else{
                 $this->Flash->error('カートに追加できません。');
             }
             return $this->redirect(['action' => 'index']);
@@ -61,8 +66,12 @@ class CartsController extends AppController
         ]);
         if ($this->request->is(['patch', 'post', 'put'])) {
             $cart = $this->Carts->patchEntity($cart, $this->request->getData());
+            if($cart->count < 0){
+                $this->Flash->error('負の数が入力されています。');
+            }
             if ($this->Carts->save($cart)) {
                 $this->Flash->success(__('変更が保存されました。'));
+                if($cart->count == 0)$this->Carts->delete($cart);
 
                 return $this->redirect(['action' => 'index']);
             }
