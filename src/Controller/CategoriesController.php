47370dee766331130dbeb855814eb2e906e11d65
Masaya Nakashima <kawa18sima@gmail.com>
2018-09-03 12:48:45 +0900

    Edit category controller
    Add user sorting function
    Japanese errors made to English errors

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Event\Event;
 
 /**
  * Categories Controller
@@ -12,12 +13,17 @@ use App\Controller\AppController;
  */
 class CategoriesController extends AppController
 {
+    public $components = ['UserLevel', 'ImageCheck'];
 
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|void
-     */
+    public function beforeFilter(Event $event){
+        parent::beforeFilter($event);
+        $this->Auth->allow([]);
+        if(!$this->UserLevel->checkUser($this->Auth->user())){
+            $this->Flash->error('アクセス権限がありません。');
+            $this->redirect(['controller' => 'products', 'action' => 'index']);
+        }
+    }
+
     public function index()
     {
         $categories = $this->paginate($this->Categories);
@@ -25,49 +31,21 @@ class CategoriesController extends AppController
         $this->set(compact('categories'));
     }
 
-    /**
-     * View method
-     *
-     * @param string|null $id Category id.
-     * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $category = $this->Categories->get($id, [
-            'contain' => ['CategoriesProduct']
-        ]);
-
-        $this->set('category', $category);
-    }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
     public function add()
     {
         $category = $this->Categories->newEntity();
         if ($this->request->is('post')) {
             $category = $this->Categories->patchEntity($category, $this->request->getData());
             if ($this->Categories->save($category)) {
-                $this->Flash->success(__('The category has been saved.'));
+                $this->Flash->success(__('カテゴリーが作成されました'));
 
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The category could not be saved. Please, try again.'));
+            $this->Flash->error(__('カテゴリーの作成に失敗しました。'));
         }
         $this->set(compact('category'));
     }
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Category id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
     public function edit($id = null)
     {
         $category = $this->Categories->get($id, [
@@ -76,30 +54,23 @@ class CategoriesController extends AppController
         if ($this->request->is(['patch', 'post', 'put'])) {
             $category = $this->Categories->patchEntity($category, $this->request->getData());
             if ($this->Categories->save($category)) {
-                $this->Flash->success(__('The category has been saved.'));
+                $this->Flash->success(__('編集が保存されました。'));
 
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The category could not be saved. Please, try again.'));
+            $this->Flash->error(__('カテゴリーの作成に失敗しました。'));
         }
         $this->set(compact('category'));
     }
 
-    /**
-     * Delete method
-     *
-     * @param string|null $id Category id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
     public function delete($id = null)
     {
         $this->request->allowMethod(['post', 'delete']);
         $category = $this->Categories->get($id);
         if ($this->Categories->delete($category)) {
-            $this->Flash->success(__('The category has been deleted.'));
+            $this->Flash->success(__('削除しました。'));
         } else {
-            $this->Flash->error(__('The category could not be deleted. Please, try again.'));
+            $this->Flash->error(__('削除に失敗しました。'));
         }
 
         return $this->redirect(['action' => 'index']);
